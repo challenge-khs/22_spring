48463a990375841ba8393819805576c798f056d4
challenge-khs <hanseong0826@gmail.com>
2022-03-06 20:00:42 +0900

    March.06(DP)

---
+++ src/baekjoon/Mar06/P2579.java
@@ -0,0 +1,26 @@
+package baekjoon.Mar06;
+
+import java.util.Scanner;
+
+public class P2579 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int score[] = new int[301];
+        int result[] = new int[301];
+
+        for(int i = 1; i <= n; i++) {
+            score[i] = sc.nextInt();
+        }
+
+        result[1] = score[1];
+        result[2] = score[1] + score[2];
+        result[3] = Math.max(score[1], score[2]) + score[3];
+
+        for(int i = 4; i <= n; i++) {
+            result[i] = Math.max(result[i - 3] + score[i - 1], result[i - 2]) + score[i];
+        }
+
+        System.out.println(result[n]);
+    }
+}
