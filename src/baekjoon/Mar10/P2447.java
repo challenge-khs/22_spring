de1a1b66650b1759b33615653459c133c324c967
challenge-khs <hanseong0826@gmail.com>
2022-03-10 19:34:40 +0900

    March.10(Recursion)

---
+++ src/baekjoon/Mar10/P2447.java
@@ -0,0 +1,35 @@
+package baekjoon.Mar10;
+
+import java.util.Scanner;
+
+public class P2447 {
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void star(int i, int j) {
+        if (i < 3 && j < 3) {
+            if(i % 3 == 1 && j % 3 == 1)
+                sb.append(" ");
+            else
+                sb.append("*");
+        }
+        else {
+            if(i % 3 == 1 && j % 3 == 1)
+                sb.append(" ");
+            else
+                star(i / 3, j / 3);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                star(i, j);
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
