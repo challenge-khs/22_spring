0c417c42e97e0cc2933aee8195543f5b9a6009ed
challenge-khs <hanseong0826@gmail.com>
2022-03-26 23:35:33 +0900

    March.26(Dynamic Programming)

---
+++ src/baekjoon/March/Mar36/P11053.java
@@ -0,0 +1,32 @@
+package baekjoon.March.Mar36;
+
+import java.util.Scanner;
+
+public class P11053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = -1;
+        for (int i = 0; i < n; i++) {
+            max = dp[i] > max ? dp[i] : max;
+        }
+
+        System.out.println(max);
+    }
+}
