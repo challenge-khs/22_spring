96d633596dcd6090a7d5ed612f5dd6d07b454721
challenge-khs <hanseong0826@gmail.com>
2022-05-01 23:38:54 +0900

    May.1(String & DP)

---
+++ src/baekjoon/May/P5525.java
@@ -0,0 +1,26 @@
+package baekjoon.May;
+
+import java.util.Scanner;
+
+public class P5525 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        char[] arr = sc.next().toCharArray();
+        int[] memo = new int[m];
+        int answer = 0;
+
+        for (int i = 1; i < m - 1; i++) {
+            if (arr[i] == 'O' && arr[i + 1] == 'I') {
+                memo[i + 1] = memo[i - 1] + 1;
+
+                if (memo[i + 1] >= n && arr[i - n * 2 + 1] == 'I') {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
