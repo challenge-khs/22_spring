02654aa6d59a5743b3987ffdbbd98549f288ba75
challenge-khs <hanseong0826@gmail.com>
2022-05-08 02:51:45 +0900

    May.7(SWEA-extra)

---
+++ src/swea/D03/P1208.java
@@ -0,0 +1,45 @@
+package swea.D03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class P1208 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = 10;
+
+        for(int test_case = 1; test_case <= T; test_case++) {
+            int dump = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+
+            PriorityQueue<Integer> minQ = new PriorityQueue<>();
+            PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+
+            for (int i = 0; i < 100; i++) {
+                int temp = Integer.parseInt(st.nextToken());
+                minQ.add(temp);
+                maxQ.add(temp);
+            }
+
+            for (int i = 0; i < dump; i++) {
+                int min = minQ.remove();
+                int max = maxQ.remove();
+
+                maxQ.remove(min);
+                minQ.add(min + 1);
+                maxQ.add(min + 1);
+
+                minQ.remove(max);
+                maxQ.add(max - 1);
+                maxQ.add(min - 1);
+            }
+
+            System.out.println("#" + test_case + " " + (maxQ.peek() - minQ.peek()));
+        }
+    }
+}
--- src/swea/D03/P1954.java
+++ src/swea/D03/P1954.java
@@ -3,43 +3,44 @@ package swea.D03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class P1954 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int T = 10;
+        int T = Integer.parseInt(br.readLine());
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
 
         for(int test_case = 1; test_case <= T; test_case++) {
-            int dump = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            int[][] arr = new int[n][n];
 
-            PriorityQueue<Integer> minQ = new PriorityQueue<>();
-            PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+            int t = 1;
+            int x = 0, y = 0, d = 0;
 
-            for (int i = 0; i < 100; i++) {
-                int temp = Integer.parseInt(st.nextToken());
-                minQ.add(temp);
-                maxQ.add(temp);
+            while (t < n * n) {
+                arr[x][y] = t;
+
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] != 0) {
+                    d = (d + 1) % 4;
+                    continue;
+                }
+                x = nx;
+                y = ny;
+                t++;
             }
+            arr[x][y] = t;
 
-            for (int i = 0; i < dump; i++) {
-                int min = minQ.remove();
-                int max = maxQ.remove();
-
-                maxQ.remove(min);
-                minQ.add(min + 1);
-                maxQ.add(min + 1);
-
-                minQ.remove(max);
-                maxQ.add(max - 1);
-                maxQ.add(min - 1);
+            System.out.println("#" + test_case);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
             }
-
-            System.out.println("#" + test_case + " " + (maxQ.peek() - minQ.peek()));
         }
     }
 }
