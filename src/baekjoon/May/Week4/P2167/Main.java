025fbe52baf946049a6cb11936474fd9c2842d4c
challenge-khs <hanseong0826@gmail.com>
2022-05-26 21:52:01 +0900

    May.26(DP)

---
+++ src/baekjoon/May/Week4/P2167/Main.java
@@ -0,0 +1,49 @@
+/*
+    문제 제목: 스타트링크
+    주소: https://www.acmicpc.net/problem/2167
+    알고리즘 분류: 구현, 누적 합
+    풀이 날짜: 2022-05-26
+ */
+
+package baekjoon.May.Week4.P2167;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n + 1][m + 1];
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[x][y] - dp[x][b - 1] - dp[a - 1][y] + dp[a - 1][b - 1]);
+        }
+    }
+}
